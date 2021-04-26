3d1c11b59cdec0845b39037c7d6200939c9a07b7
bsorrentino <bartolomeo.sorrentinogmail.com>
2021-04-26 17:03:39 +0200

    update

--- packages/rxbus/lib/index.d.ts
+++ packages/rxbus/lib/index.d.ts
@@ -1,18 +1,17 @@
 import * as bus from 'bus-module';
 import { Observable, Subject } from 'rxjs';
-
 export declare namespace MessageBus {
-    export class Channels {
+    class Channels {
         private _channels;
         newChannel<T>(name: string): Subject<T>;
         channel<T>(name: string): Observable<T>;
     }
-    export class Modules {
+    class Modules {
         private _modules;
         registerModule(module: bus.Module): void;
         start(): void;
     }
-    export class Engine {
+    class Engine {
         readonly channels: Channels;
         readonly modules: Modules;
     }
--- packages/rxsample/src/rxsample.ts
+++ packages/rxsample/src/rxsample.ts
@@ -1,2 +1,73 @@
-import {Bus, MessageBus} from 'rxbus'
 
+import { Bus } from 'rxbus'
+import { MessageBus } from 'bus-module'
+import { interval, Subject, Subscription } from 'rxjs'
+import { tap } from 'rxjs/operators'
+
+class MyModule implements MessageBus.Module {
+
+    readonly name = "MyModule"
+    
+    private _myChannel?:Subject<string>
+    private _subscription?:Subscription
+
+    get myChannel() { return this._myChannel }
+    
+    onRegister() {
+        this._myChannel = Bus.channels.newChannel( `${this.name}/channel` )
+    }
+
+    onStart() {
+        this._subscription = 
+            Bus.channels.channel( 'TimerModule/channel')
+                .subscribe( { next: time => console.log( 'time', time) })
+    }
+
+    onStop() {
+        if( this._subscription) {
+            this._subscription.unsubscribe()
+            this._subscription = undefined
+        }
+    }
+}
+
+class TimerModule implements MessageBus.Module {
+
+    readonly name = "TimeModule"
+    
+    private _myChannel?:Subject<number>
+
+    get myChannel() { return this._myChannel }
+    private _subscription?:Subscription
+
+    onRegister() {
+        this._myChannel = Bus.channels.newChannel( `${this.name}/channel` )
+    }
+
+    onStart() {
+        this._subscription = interval(1000)
+            .pipe( tap( tick => console.log( 'tick', tick )) )
+            .subscribe( this._myChannel )
+    }
+
+    onStop() {
+        if( this._subscription ) {
+            this._subscription.unsubscribe()
+            this._subscription = undefined
+        }
+    }
+}
+
+
+function main() {
+
+    Bus.modules.registerModule( new MyModule() )
+    Bus.modules.registerModule( new TimerModule() )
+
+
+    Bus.modules.start()
+
+
+}
+
+main()
