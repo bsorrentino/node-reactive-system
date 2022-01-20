20eb0579a273ef97d159f39c595cd226991f543f
Build Pipeline <build.pipeline@softphone.it>
2022-01-20 16:57:29 +0100

    add 'lib' to ignore

--- packages/rxbus-trace/lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as bus from '@bsorrentino/bus-core';
-declare class TraceModule implements bus.Module {
-    readonly name = "TRACE";
-    private _subscriptions;
-    onStart(): void;
-    onStop(): void;
-}
-export declare const Module: TraceModule;
-export {};
--- packages/rxmq/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { EndlessSubject, EndlessReplaySubject } from './rx/subjects';
-export { ChannelEvent, RequestOptions, Channel, RequestResponseChannel } from './channel';
-export { Rxmq } from './rxmq';
