032a3fbbb19b85e78423e804bd0a33749fb087e5
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-07-08 15:09:44 +0000

    test: test EVT use cases

--- packages/evt-bus/src/index.ts
+++ packages/evt-bus/src/index.ts
@@ -1,4 +1,4 @@
-import { Evt, to } from "evt";
+import { Evt, EvtError, to } from "evt";
 //Or import { Evt } from "https://evt.land/x/evt/mod.ts" on deno
 
 function snippet1() {
@@ -13,7 +13,6 @@ function snippet1() {
     evtTime.post(1234);
 }
 
-
 function snippet2() {
 
     type Data = [ "TEXT",  string ] | [ "TIME",  number ];
@@ -27,7 +26,6 @@ function snippet2() {
     evt.post(["TIME", 123]);
     evt.post(["TIME", 1234]);   
 }
-
 
 async function snippet3() {
     type Data = {
@@ -58,6 +56,66 @@ async function snippet3() {
     clearInterval( interval )
 }
 
+interface EventIterator<T> extends AsyncIterable<T>  {
+    stop(): void
+}
+
+function waitForEvents<T>(  evt: Evt<T> , timeout?: number ): EventIterator<T> {
+
+    let isStopped = false
+    async function* events() {
+        
+        while(!isStopped) {
+
+            try{
+                const res =  await evt.waitFor( timeout )
+                yield res
+
+            } catch(error) {
+                console.warn("TIMEOUT!");
+                isStopped = true
+            }
+        }
+    }
+
+    return {
+        [Symbol.asyncIterator]: () => events(),
+
+        stop: () => isStopped = true
+    }
+}
+
+async function snippet4() {
+
+    type Data = {
+        type: "TEXT";
+        text: string;
+    } | {
+        type: "AGE";
+        age: number;
+    }
+
+    const evt = new Evt<Data>()
+    
+    let numEvents = 0;
+    const interval = setInterval( () => {
+        evt.post( { type: 'TEXT', text: 'HI THERE!'})
+        if( ++numEvents==10 )  clearInterval(interval)
+    }, 2000);
+
+    const eventIterator = waitForEvents( evt, 5000 ) 
+
+    for await ( const ev of eventIterator ) {
+    
+        console.log( numEvents, ev )
+    
+        
+    }
+}
+
+
+
 snippet1()
 snippet2()
-snippet3()
+// snippet3()
+snippet4()
